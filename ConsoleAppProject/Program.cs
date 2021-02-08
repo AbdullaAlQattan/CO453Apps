a56f3db927c38dbbc2d81b46b3c2ea340a1abc72
AbdullaAlQattan <72339250+AbdullaAlQattan@users.noreply.github.com>
2021-02-08 14:51:51 +0300

    edited my name

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -8,16 +8,17 @@ namespace ConsoleAppProject
     /// when the application is started.  It will be used
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
-    /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// This Project has been modified by:Abdulla AlQattan
+    /// Abdulla AlQattan 14/12/2020
     /// </summary>
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
             
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+            Console.WriteLine("Abdulla AlQattan");
             Console.WriteLine();
 
             Console.WriteLine(MyEnum.FirstValue.EnumValue());
