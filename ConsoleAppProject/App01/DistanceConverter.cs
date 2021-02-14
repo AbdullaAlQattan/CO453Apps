bdd27e2f4aa05c44fe8c340bf7ab3d33b1f93910
AbdullaAlQattan <72339250+AbdullaAlQattan@users.noreply.github.com>
2021-02-14 21:13:46 +0300

    Update DistanceConverter.cs

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -4,10 +4,37 @@
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Abdulla AlQattan version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+        /// <summary>
+        ///  Prompt the user to enter the distance in miles
+        ///  Input the miles as a double number
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        private void InputMiles()
+        {
+
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
 
     }
 }
