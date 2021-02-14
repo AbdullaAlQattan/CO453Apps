16dc43d72fbe29323bff65dbfca77ae414e2eb22
AbdullaAlQattan <72339250+AbdullaAlQattan@users.noreply.github.com>
2021-02-14 21:48:20 +0300

    Distance Converter updated

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,6 +10,8 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
+
         private double miles;
 
         private double feet;
@@ -20,9 +22,18 @@ namespace ConsoleAppProject.App01
         /// </summary>
         public void Run()
         {
+            OutputHeading();
             InputMiles();
             CalculateFeet();
             OutputFeet();
+        }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine("         Convert Miles to Feet       ");
+            Console.WriteLine("         by Abdulla AlQattan         ");
+            Console.WriteLine("-------------------------------------\n");
         }
 
         private void InputMiles()
@@ -34,12 +45,12 @@ namespace ConsoleAppProject.App01
 
         private void CalculateFeet()
         {
-
+            feet = miles * 5280;
         }
 
         private void OutputFeet()
         {
-
+            Console.WriteLine(miles + " miles is " + " feet!");
         }
 
     }
